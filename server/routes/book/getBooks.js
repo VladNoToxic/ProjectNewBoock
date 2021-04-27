20b30125c7f0e46ee7462380cb5635ff1f0ca331
Vlad Gladskih <gladskih.vlad@mail.ru>
2021-04-27 21:37:59 +0300

    11

--- server/routes/book/getBooks.js
+++ server/routes/book/getBooks.js
@@ -25,7 +25,7 @@ module.exports = async function(fastify) {
 			benchmark: true,
 			rejectOnEmpty: true
 		};
-		if (request.query.sortBy.length > 0) opts.order = [[request.query.sortBy]];
+		if (request.query.sortBy && request.query.sortBy.length > 0) opts.order = [[request.query.sortBy]];
 		if (request.query.sortDesc == 'true') opts.order[0].push('DESC');
 
 		await global.app.Database.book.findAndCountAll(opts).then(result => {
